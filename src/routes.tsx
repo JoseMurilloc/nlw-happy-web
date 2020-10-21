a5d41b5942f7a9f0374c567f597af3cb60427f85
Jose Murillo <jooseemurillo@gmail.com>
2020-10-21 15:47:57 -0300

    Update package 🌐

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import CreateOrphanage from './pages/CreateOrphanage';
 import Lading from './pages/Landing';
+import Login from './pages/Login';
 import Orphanage from './pages/Orphanage';
 import OrphanagesMap from './pages/OrphanagesMap';
 
@@ -14,6 +15,8 @@ const Routes: React.FC = () => {
         
         <Route path="/orphanages/create" component={CreateOrphanage} />
         <Route path="/organages/:id" component={Orphanage} />
+        
+        <Route path="/login" component={Login} />
       </Switch>
     </BrowserRouter>
   )
