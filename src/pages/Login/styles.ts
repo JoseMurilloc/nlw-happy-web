2b2173bdafd691b22f896104ed5fb42ade1a4213
Jose Murillo <jooseemurillo@gmail.com>
2020-10-21 15:39:53 -0300

    remove css input on login

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -58,26 +58,6 @@ export const Form = styled.form`
     margin-top: 16px;
   }
 
-  > input {
-    width: 360px;
-    height: 64px;
-    
-    border: 1px solid #D3E2E5;
-    box-sizing: border-box;
-    border-radius: 20px;
-
-    background: #F5F8FA;
-
-    padding: 21px 24px;
-
-    color: #5C8599;
-    font-weight: 600;
-    font-size: 16px;
-    line-height: 22px;
-
-
-  }
-
   > div {
     width: 100%;
     margin-bottom: 40px;
