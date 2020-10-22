0ab8bfe2d2aa6fd67af78a0130101bc9b6078b29
Jose Murillo <jooseemurillo@gmail.com>
2020-10-22 11:57:10 -0300

    ⬆️⬆️⬆️

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+
+import { Container, Main, ContainerListOrphanageGrip, ContainerOrphanage } from './styles';
+
+import logoImage from '../../images/map-marker.svg'
+import { Link } from 'react-router-dom';
+
+import localMarker from '../../images/local-marker.svg';
+import alertCircle from '../../images/alert-cicle.svg';
+import close from '../../images/close.svg';
+import mapa from '../../images/mapa.svg';
+import notFoundOrphanages from '../../images/not-found-orphanages.svg';
+
+import {  FiEdit3 } from 'react-icons/fi';
+import { RiDeleteBin7Line } from 'react-icons/ri';
+
+
+const Dashboard: React.FC = () => {
+
+  const [orphanages, Orphanages] = useState(false);
+
+  return (
+    <Container>
+      <header>
+        <img src={logoImage} alt="logo happy"/>
+
+        <div>
+          <Link to="/">
+            <img src={localMarker} alt=""/>
+          </Link>
+          <Link to="/">
+            <img src={alertCircle} alt=""/>
+          </Link>
+        </div>
+
+        <button>
+          <img src={close} alt=""/>
+        </button>
+      </header>
+
+      <Main orphanages={orphanages}>
+        <header>
+          <h1>Orfanatos Cadastrados</h1>
+          <span>2 ofanatos</span>
+        </header>
+
+        {orphanages ? (
+          <ContainerListOrphanageGrip>
+          <ContainerOrphanage>
+              <img src={mapa} alt=""/>
+             
+              <footer>
+                <h1>Orf. Esperança</h1>
+                  
+                  <div>
+                    <Link to="/">
+                      <RiDeleteBin7Line color="#15C3D6" size={30}/>
+                    </Link>
+                    <Link to="/">
+                      <FiEdit3 color="#15C3D6" size={30}/>
+                    </Link>   
+                  </div>   
+              </footer>  
+             
+          </ContainerOrphanage>
+
+          <ContainerOrphanage>
+              <img src={mapa} alt=""/>
+             
+              <footer>
+                <h1>Orf. Esperança</h1>
+                  
+                  <div>
+                    <Link to="/">
+                      <RiDeleteBin7Line color="#15C3D6" size={30}/>
+                    </Link>
+                    <Link to="/">
+                      <FiEdit3 color="#15C3D6" size={30}/>
+                    </Link>   
+                  </div>   
+              </footer>  
+             
+          </ContainerOrphanage>
+          
+        
+          </ContainerListOrphanageGrip>
+        ) : (
+          <div>
+            <img src={notFoundOrphanages} alt="Não encontrado orfanatos"/>
+          </div>
+        )}
+      </Main>
+    </Container>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,117 @@
+import styled, { css } from 'styled-components';
+
+interface MainProps {
+  orphanages: boolean;
+}
+
+export const Container = styled.div`
+
+  display: flex;
+
+  > header {
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    background: #15C3D6;
+
+    width: 96px;
+    height: 100vh;
+
+    padding: 32px 0;
+
+    img {
+      width: 50px;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column; 
+      align-items: center;
+    }
+
+    button {
+      background-color: transparent;
+      border: 0;
+    }
+  }
+`;
+
+export const Main = styled.main<MainProps>`
+
+  width: 100%;
+  
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 122px;
+
+    margin: 0 120px;
+    border-bottom: 1px solid #D3E2E5;
+    
+    
+  }
+
+  h1 {
+    color: #4D6F80;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 34px;
+  }
+
+  span {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    color: #8FA7B3;
+  }
+
+  ${props => !props.orphanages && css`
+    div { 
+      width: 100%;
+      height: 487px;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  `}
+`;
+
+export const ContainerListOrphanageGrip = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 0 100px;
+  margin-top: 40px;
+`;
+
+export const ContainerOrphanage = styled.div`
+  width: 544px;
+  height: 227px;
+  box-sizing: border-box;
+  margin-right: 32px;
+
+
+  footer {
+    background-color: #fff;
+    height: 82px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 0 32px;
+
+    a {
+      background: #EBF2F5;
+      border-radius: 16px;
+      width: 48px;
+      height: 48px;
+      padding: 15px;
+
+    }
+  }
+`;
